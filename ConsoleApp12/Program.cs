449f371759b37ff66d124db2eb38d2af01e2ba28
ADMIN <ADMIN@VISH>
2024-01-28 23:10:00 +0530

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            Console.WriteLine(myNum);
+            double myDoubleNum = 5.99D;
+            Console.WriteLine(myDoubleNum);
+            char myLetter = 'D';
+            Console.WriteLine(myLetter);   
+            bool myBool = false;
+            Console.WriteLine(myBool);
+            String myText = "Hello";
+            Console.WriteLine(myText);
+      
+        
+        }
+    }
+}
